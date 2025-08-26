5ceb06c1813f0cd57f09f0ca34734ad5db357686
Isabel Rosário <isabel.rosario102000@gmail.com>
2025-08-26 11:33:32 +0100

    base table setup; starting to texture things

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'
+import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader.js'
+
+// --- loaders
+const manager = new THREE.LoadingManager()
+const texLoader = new THREE.TextureLoader(manager)
+const exrLoader = new EXRLoader(manager)
+const hdrLoader = new RGBELoader(manager)
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -15,8 +23,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -48,18 +55,142 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// add texture as scene background
+const backgroundTexture = texLoader.load('textures/graph_paper.jpg')
+scene.background = backgroundTexture
+backgroundTexture.wrapS = THREE.RepeatWrapping
+backgroundTexture.wrapT = THREE.RepeatWrapping
+backgroundTexture.repeat.set(3, 3)
+
+/**
+ * Lighting
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
+
+const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.5)
+scene.add(hemisphereLight)
+
+const directionalLight = new THREE.DirectionalLight(0xfff4c9, 0.5)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
+
+const directionalLight2 = new THREE.DirectionalLight(0xffffff, 1)
+directionalLight2.position.set(-1, -1, 0)
+scene.add(directionalLight2)
+
+
+// HDR texture
+hdrLoader.load('textures/pine_attic_4k.hdr', (texture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping
+    scene.environment = texture
+})
+
+
+/**
+ * Coffee table
+ */
+
+const topWidth = 1
+const baseHeight = 0.8
+const baseTopWidth = 0.45
+const baseBottomWidth = 0.7
+const bevel = 0.01
+const bevelHeight = 0.05
+
+const coffeeTable = new THREE.Group()
+
+const tableTopGeom = new THREE.CylinderGeometry(topWidth, topWidth - bevel, bevelHeight, 64)
+// TODO: fix UV mapping
+const tableTopMat = new THREE.MeshStandardMaterial({ color: 0x7f7f7f, roughness: 0.5, metalness: 0.1 })
+const tableTop = new THREE.Mesh(tableTopGeom, tableTopMat)
+tableTop.name = "tableTop"
+tableTop.position.y = (baseHeight + bevelHeight) / 2
+
+const tableBaseGeom = new THREE.CylinderGeometry(baseTopWidth, baseBottomWidth, baseHeight, 64)
+const tableBaseMat = new THREE.MeshStandardMaterial({ color: 0x7f7f7f, roughness: 0.5, metalness: 0.1 })
+const tableBase = new THREE.Mesh(tableBaseGeom, tableBaseMat)
+
+const tableFooterGeom = new THREE.CylinderGeometry(baseBottomWidth, baseBottomWidth - bevel, bevelHeight, 64)
+const tableFooter = new THREE.Mesh(tableFooterGeom, tableBaseMat)
+tableFooter.position.y = - (baseHeight + bevelHeight) / 2
+
+coffeeTable.add(tableTop)
+coffeeTable.add(tableBase)
+coffeeTable.add(tableFooter)
+scene.add(coffeeTable)
+
+const woodTextureBasePath = '/textures/wood_table/'
+const woodTextureFiles = {
+    ao: woodTextureBasePath + 'wood_table_001_ao_4k.jpg',
+    arm: woodTextureBasePath + 'wood_table_001_arm_4k.jpg',
+    diff: woodTextureBasePath + 'wood_table_001_diff_4k.jpg',
+    disp: woodTextureBasePath + 'wood_table_001_disp_4k.png',
+    normalEXR: woodTextureBasePath + 'wood_table_001_nor_gl_4k.exr',
+    roughEXR: woodTextureBasePath + 'wood_table_001_rough_4k.exr'
+}
+const woodTextureMaps = {}
+
+// load standard images
+texLoader.load(woodTextureFiles.diff, t => {
+    t.encoding = THREE.sRGBEncoding
+    t.wrapS = t.wrapT = THREE.RepeatWrapping
+    woodTextureMaps.map = t
+})
+texLoader.load(woodTextureFiles.ao, t => { t.wrapS = t.wrapT = THREE.RepeatWrapping; woodTextureMaps.aoMap = t })
+texLoader.load(woodTextureFiles.disp, t => { t.wrapS = t.wrapT = THREE.RepeatWrapping; woodTextureMaps.displacementMap = t })
+texLoader.load(woodTextureFiles.arm, t => { t.wrapS = t.wrapT = THREE.RepeatWrapping; woodTextureMaps.arm = t })
+
+// load EXR maps
+exrLoader.load(woodTextureFiles.normalEXR, tex => {
+    tex.encoding = THREE.LinearEncoding
+    tex.flipY = false
+    woodTextureMaps.normalMap = tex
+})
+exrLoader.load(woodTextureFiles.roughEXR, tex => {
+    tex.encoding = THREE.LinearEncoding
+    tex.flipY = false
+    woodTextureMaps.roughnessMap = tex
+})
+
+// when all loaded, create material and assign it
+manager.onLoad = () => {
+    const geom = tableTop.geometry
+    if (!geom.attributes.uv2) {
+        geom.setAttribute('uv2', new THREE.BufferAttribute(geom.attributes.uv.array.slice(), 2))
+    }
+
+    const material = new THREE.MeshStandardMaterial({
+        map: woodTextureMaps.map || null,
+        aoMap: woodTextureMaps.aoMap || woodTextureMaps.arm || null,
+        normalMap: woodTextureMaps.normalMap || null,
+        roughnessMap: woodTextureMaps.roughnessMap || woodTextureMaps.arm || null,
+        displacementMap: woodTextureMaps.displacementMap || null,
+        metalness: 0.0,
+        roughness: 1.0,
+        displacementScale: 0,
+        aoMapIntensity: 1.0
+    })
+
+    if (material.map) material.map.encoding = THREE.sRGBEncoding
+    if (material.normalMap) material.normalMap.flipY = false
+
+    tableTop.material = material
+    tableTop.material.needsUpdate = true
+}
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
 
     // Update controls
